2942a007b9cd637809c66d69aefb442d590c87fd
AStewart505 <138970386+AStewart505@users.noreply.github.com>
2024-04-17 21:02:55 +0100

    Add files via upload

---
+++ App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+    Elf Elf1 = new Elf(1,1,3,4,5,6,7,"Elf","Plains","Black Heart", "High");
+
+
+    Elf1.toString();
+    }
+}
